8349e665bd85ea03f9c4f0db84131ed5121890c9
Thiago Souza <thiagorasouza@gmail.com>
2022-11-18 16:52:22 -0300

    feat: ensure sign up routes return an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,16 @@
+import request from "supertest";
+import app from "../config/app";
+
+describe("SignUp Routes", () => {
+  it("should return an account on success", async () => {
+    await request(app)
+      .post("/api/signup")
+      .send({
+        name: "valid_name",
+        email: "valid_email",
+        password: "valid_password",
+        passwordConfirmation: "valid_password",
+      })
+      .expect(200);
+  });
+});
