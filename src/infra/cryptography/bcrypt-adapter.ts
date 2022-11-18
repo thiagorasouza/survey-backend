065b2467cf78f95fa7a851dca79003ec6e4ad8e9
Thiago Souza <thiagorasouza@gmail.com>
2022-11-17 22:38:29 -0300

    feat: ensure that Bcrypt Adapter calls bcryp with correct value

---
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -0,0 +1,9 @@
+import * as bcrypt from "bcrypt";
+import { Encrypter } from "../../data/protocols/encrypter";
+
+export class BcryptAdapter implements Encrypter {
+  constructor(private readonly salt: number) {}
+  async encrypt(value: string): Promise<string> {
+    return bcrypt.hash(value, 12);
+  }
+}
