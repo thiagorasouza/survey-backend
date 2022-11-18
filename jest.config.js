1b71652f051308f3364749b6df4229cd5c8decb5
Thiago Souza <thiagorasouza@gmail.com>
2022-11-18 15:23:55 -0300

    chore: configure jest to work with mongodb in memory

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-// /** @type {import('ts-jest').JestConfigWithTsJest} */
-const tsJestPreset = require("ts-jest/jest-preset");
-const jestMongoDbPreset = require("@shelf/jest-mongodb/jest-preset");
-
 module.exports = {
-  ...tsJestPreset,
-  ...jestMongoDbPreset,
+  roots: ["<rootDir>/src"],
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
   testEnvironment: "node",
+  preset: "@shelf/jest-mongodb",
 };
