e9c984fe8c61cff723a54d03f89aec1d8db58ae5
Thiago Souza <thiagorasouza@gmail.com>
2022-11-18 21:23:10 -0300

    feat: connect to mongodb before server starts

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,7 @@
+import { MongoHelper } from "../infra/db/mongodb/helpers/mongo-helper";
 import app from "./config/app";
+import env from "./config/env";
 
-app.listen(5000, () => console.log("Sever running"));
+MongoHelper.connect(env.mongoUrl).then(() => {
+  app.listen(env.port, () => console.log("Sever running"));
+});
