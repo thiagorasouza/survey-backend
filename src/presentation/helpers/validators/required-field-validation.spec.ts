1291b0d7951acc944b32fde170ff7c42305206f2
Thiago Souza <thiagorasouza@gmail.com>
2022-11-21 16:56:12 -0300

    test: ensure that RequiredFieldValidation returns nothing if field is present

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe("RequiredField Validations", () => {
     const result = sut.validate({});
     expect(result).toEqual(new MissingParamError("any_field"));
   });
+
+  it("should return nothing if field is present", () => {
+    const sut = new RequiredFieldValidation("any_field");
+    const result = sut.validate({ any_field: "any_value" });
+    expect(result).toBeUndefined();
+  });
 });
