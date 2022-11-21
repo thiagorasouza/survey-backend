14ad032df0e5eebf110488e0fb2a4cbf99a64b0d
Thiago Souza <thiagorasouza@gmail.com>
2022-11-21 17:09:04 -0300

    feat: ensure that ValidationComposite returns an error if validaiton fails

---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,34 @@
+import { MissingParamError } from "../../errors";
+import { Validation } from "./validation";
+import { ValidationComposite } from "./validation-composite";
+
+interface SutTypes {
+  sut: ValidationComposite;
+  validationStub: Validation;
+}
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate(input: unknown): Error {
+      return null;
+    }
+  }
+  return new ValidationStub();
+};
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation();
+  const sut = new ValidationComposite([validationStub]);
+  return { sut, validationStub };
+};
+
+describe("Validation Composite", () => {
+  it("should return an error if any validation fails ", () => {
+    const { sut, validationStub } = makeSut();
+    jest
+      .spyOn(validationStub, "validate")
+      .mockReturnValueOnce(new MissingParamError("field"));
+    const error = sut.validate({ other_field: "any_value" });
+    expect(error).toEqual(new MissingParamError("field"));
+  });
+});
