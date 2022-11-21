5c7d88004f944d5aabcb27e4e38a4b014b72ef43
Thiago Souza <thiagorasouza@gmail.com>
2022-11-21 11:26:40 -0300

    feat: ensure that SignUpController calls Validation with correct values

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -9,6 +9,7 @@ import {
   AddAccount,
   AddAccountModel,
   HttpRequest,
+  Validation,
 } from "./signup-protocols";
 import { SignUpController } from "./signup";
 import { badRequest, ok, serverError } from "../../helpers/http-helper";
@@ -35,18 +36,34 @@ const makeAddAccount = (): AddAccount => {
   return new AddAccountStub();
 };
 
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate(input: unknown): Error {
+      return null;
+    }
+  }
+
+  return new ValidationStub();
+};
+
 interface SutTypes {
   sut: SignUpController;
   emailValidatorStub: EmailValidator;
   addAccountStub: AddAccount;
+  validationStub: Validation;
 }
 
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidator();
   const addAccountStub = makeAddAccount();
-  const sut = new SignUpController(emailValidatorStub, addAccountStub);
+  const validationStub = makeValidation();
+  const sut = new SignUpController(
+    emailValidatorStub,
+    addAccountStub,
+    validationStub
+  );
 
-  return { sut, emailValidatorStub, addAccountStub };
+  return { sut, emailValidatorStub, addAccountStub, validationStub };
 };
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -202,5 +219,13 @@ describe("SignUp Controller", () => {
     const httpRequest = makeFakeRequest();
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(ok(makeFakeAccount()));
+  });
+
+  test("Should call Validation with correct value", async () => {
+    const { sut, validationStub } = makeSut();
+    const validate = jest.spyOn(validationStub, "validate");
+    const httpRequest = makeFakeRequest();
+    await sut.handle(httpRequest);
+    expect(validate).toHaveBeenCalledWith(httpRequest.body);
   });
 });
