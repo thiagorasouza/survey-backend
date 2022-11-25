80186d54361648addd67b0e20d77b4fd9aeaeeee
Thiago Souza <thiagorasouza@gmail.com>
2022-11-24 23:30:30 -0300

    fix: fix HashComparer interface

--- src/data/protocols/criptography/hash-comparer.ts
+++ src/data/protocols/criptography/hash-comparer.ts
@@ -1,3 +1,3 @@
-class HashComparer {
+export interface HashComparer {
   compare(value: string, hash: string): Promise<boolean>;
 }
--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,6 +2,7 @@ import {
   Authentication,
   AuthenticationModel,
 } from "../../../domain/usecases/authentication";
+import { HashComparer } from "../../protocols/criptography/hash-comparer";
 import { LoadAccountByEmailRepository } from "../../protocols/db/load-account-by-email-repository";
 
 export class DbAuthentication implements Authentication {
