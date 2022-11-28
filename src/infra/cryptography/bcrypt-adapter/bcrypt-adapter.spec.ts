e17132d1e69bc08437fe84235b556ed42e565fd1
Thiago Souza <thiagorasouza@gmail.com>
2022-11-28 20:44:54 -0300

    refactor: separate tests by interface implementation using jest describe blocks

--- src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.spec.ts
@@ -19,58 +19,62 @@ const makeSut = (): BcryptAdapter => {
 };
 
 describe("Bcrypt Adapter", () => {
-  it("should call hash with correct values", async () => {
-    const sut = makeSut();
-    const hashSpy = jest.spyOn(bcrypt, "hash");
-    await sut.hash("any_value");
+  describe("Hasher", () => {
+    it("should call hash with correct values", async () => {
+      const sut = makeSut();
+      const hashSpy = jest.spyOn(bcrypt, "hash");
+      await sut.hash("any_value");
 
-    expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
-  });
-
-  it("should return a valid hash on hash success", async () => {
-    const sut = makeSut();
-    const hash = await sut.hash("any_value");
-    expect(hash).toBe("hashed_value");
-  });
-
-  it("should throw if hash throws", async () => {
-    const sut = makeSut();
-    jest.spyOn(bcrypt, "hash").mockImplementation(async () => {
-      throw new Error();
+      expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
     });
-    const promise = sut.hash("any_value");
-    expect(promise).rejects.toThrow();
-  });
 
-  it("should call compare with correct values", async () => {
-    const sut = makeSut();
-    const compareSpy = jest.spyOn(bcrypt, "compare");
-    await sut.compare("any_value", "any_hash");
-
-    expect(compareSpy).toHaveBeenCalledWith("any_value", "any_hash");
-  });
-
-  it("should return true when compare succeeds", async () => {
-    const sut = makeSut();
-    const result = await sut.compare("any_value", "any_hash");
-    expect(result).toBe(true);
-  });
-
-  it("should return false when compare fails", async () => {
-    const sut = makeSut();
-    jest.spyOn(bcrypt, "compare").mockImplementationOnce(async () => {
-      return false;
+    it("should return a valid hash on hash success", async () => {
+      const sut = makeSut();
+      const hash = await sut.hash("any_value");
+      expect(hash).toBe("hashed_value");
     });
-    const result = await sut.compare("any_value", "any_hash");
-    expect(result).toBe(false);
+
+    it("should throw if hash throws", async () => {
+      const sut = makeSut();
+      jest.spyOn(bcrypt, "hash").mockImplementation(async () => {
+        throw new Error();
+      });
+      const promise = sut.hash("any_value");
+      expect(promise).rejects.toThrow();
+    });
   });
 
-  it("should throw if compare throws", async () => {
-    const sut = makeSut();
-    jest.spyOn(bcrypt, "compare").mockImplementation(async () => {
-      throw new Error();
+  describe("HashComparer", () => {
+    it("should call compare with correct values", async () => {
+      const sut = makeSut();
+      const compareSpy = jest.spyOn(bcrypt, "compare");
+      await sut.compare("any_value", "any_hash");
+
+      expect(compareSpy).toHaveBeenCalledWith("any_value", "any_hash");
     });
-    const promise = sut.compare("any_value", "any_hash");
-    expect(promise).rejects.toThrow();
+
+    it("should return true when compare succeeds", async () => {
+      const sut = makeSut();
+      const result = await sut.compare("any_value", "any_hash");
+      expect(result).toBe(true);
+    });
+
+    it("should return false when compare fails", async () => {
+      const sut = makeSut();
+      jest.spyOn(bcrypt, "compare").mockImplementationOnce(async () => {
+        return false;
+      });
+      const result = await sut.compare("any_value", "any_hash");
+      expect(result).toBe(false);
+    });
+
+    it("should throw if compare throws", async () => {
+      const sut = makeSut();
+      jest.spyOn(bcrypt, "compare").mockImplementation(async () => {
+        throw new Error();
+      });
+      const promise = sut.compare("any_value", "any_hash");
+      expect(promise).rejects.toThrow();
+    });
   });
 });
--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -21,55 +21,60 @@ describe("Account Mongo Repository", () => {
   afterAll(async () => {
     await MongoHelper.disconnect();
   });
-
-  it("should return an account on add success", async () => {
-    const sut = makeSut();
-    const account = await sut.add({
-      name: "any_name",
-      email: "any_email",
-      password: "any_password",
+  describe("AddAccountRepository", () => {
+    it("should return an account on add success", async () => {
+      const sut = makeSut();
+      const account = await sut.add({
+        name: "any_name",
+        email: "any_email",
+        password: "any_password",
+      });
+      expect(account).toBeTruthy();
+      expect(account.id).toBeTruthy();
+      expect(account.name).toBe("any_name");
+      expect(account.email).toBe("any_email");
+      expect(account.password).toBe("any_password");
     });
-    expect(account).toBeTruthy();
-    expect(account.id).toBeTruthy();
-    expect(account.name).toBe("any_name");
-    expect(account.email).toBe("any_email");
-    expect(account.password).toBe("any_password");
   });
 
-  it("should return an account on loadByEmail success", async () => {
-    const sut = makeSut();
-    await accounts.insertOne({
-      name: "any_name",
-      email: "any_email@email.com",
-      password: "any_password",
+  describe("LoadAccountByEmailRepository", () => {
+    it("should return an account on loadByEmail success", async () => {
+      const sut = makeSut();
+      await accounts.insertOne({
+        name: "any_name",
+        email: "any_email@email.com",
+        password: "any_password",
+      });
+      const account = await sut.loadByEmail("any_email@email.com");
+      expect(account).toBeTruthy();
+      expect(account.id).toBeTruthy();
+      expect(account.name).toBe("any_name");
+      expect(account.email).toBe("any_email@email.com");
+      expect(account.password).toBe("any_password");
     });
-    const account = await sut.loadByEmail("any_email@email.com");
-    expect(account).toBeTruthy();
-    expect(account.id).toBeTruthy();
-    expect(account.name).toBe("any_name");
-    expect(account.email).toBe("any_email@email.com");
-    expect(account.password).toBe("any_password");
+
+    it("should return null on loadByEmail fails", async () => {
+      const sut = makeSut();
+      const account = await sut.loadByEmail("any_email@email.com");
+      expect(account).toBeFalsy();
+    });
   });
 
-  it("should return null on loadByEmail fails", async () => {
-    const sut = makeSut();
-    const account = await sut.loadByEmail("any_email@email.com");
-    expect(account).toBeFalsy();
-  });
+  describe("UpdateAccessTokenRepository", () => {
+    it("should update the account accessToken on updateAccessToken success", async () => {
+      const sut = makeSut();
 
-  it("should update the account accessToken on updateAccessToken success", async () => {
-    const sut = makeSut();
+      const result = await accounts.insertOne({
+        name: "any_name",
+        email: "any_email@email.com",
+        password: "any_password",
+      });
+      const { insertedId } = result;
 
-    const result = await accounts.insertOne({
-      name: "any_name",
-      email: "any_email@email.com",
-      password: "any_password",
+      await sut.updateAccessToken(insertedId.toString(), "any_token");
+      const account = await accounts.findOne({ _id: insertedId });
+
+      expect(account).toBeTruthy();
     });
-    const { insertedId } = result;
-
-    await sut.updateAccessToken(insertedId.toString(), "any_token");
-    const account = await accounts.findOne({ _id: insertedId });
-
-    expect(account).toBeTruthy();
   });
 });
