143ae38c997d50803535144ad5a2344fd751f102
Thiago Souza <thiagorasouza@gmail.com>
2022-11-28 20:51:08 -0300

    feat: ensure that JwtAdapter calls jwt.verify with correct values

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -3,27 +3,42 @@ import { JwtAdapter } from "./jwt-adapter";
 
 const makeSut = (): JwtAdapter => new JwtAdapter("secret");
 
-describe("JWT Adapter", () => {
-  it("should call sign with correct values", async () => {
-    const sut = makeSut();
-    const signSpy = jest.spyOn(jwt, "sign");
-    await sut.encrypt("any_id");
-    expect(signSpy).toHaveBeenCalledWith({ id: "any_id" }, "secret");
-  });
-
-  it("should return a token on sign success", async () => {
-    const sut = makeSut();
-    jest.spyOn(jwt, "sign").mockImplementationOnce(async () => "any_token");
-    const accessToken = await sut.encrypt("any_id");
-    expect(accessToken).toBe("any_token");
-  });
-
-  it("should throw if sign throws", async () => {
-    const sut = makeSut();
-    jest.spyOn(jwt, "sign").mockImplementationOnce(async () => {
-      throw new Error();
+describe("JWT Adapter Encrypter", () => {
+  describe("Encrypter", () => {
+    it("should call sign with correct values", async () => {
+      const sut = makeSut();
+      const signSpy = jest.spyOn(jwt, "sign");
+      await sut.encrypt("any_id");
+      expect(signSpy).toHaveBeenCalledWith({ id: "any_id" }, "secret");
     });
-    const promise = sut.encrypt("any_id");
-    expect(promise).rejects.toThrow();
+
+    it("should return a token on sign success", async () => {
+      const sut = makeSut();
+      jest.spyOn(jwt, "sign").mockImplementationOnce(async () => "any_token");
+      const accessToken = await sut.encrypt("any_id");
+      expect(accessToken).toBe("any_token");
+    });
+
+    it("should throw if sign throws", async () => {
+      const sut = makeSut();
+      jest.spyOn(jwt, "sign").mockImplementationOnce(async () => {
+        throw new Error();
+      });
+      const promise = sut.encrypt("any_id");
+      expect(promise).rejects.toThrow();
+    });
+  });
+
+  describe("Decrypter", () => {
+    it("should call verify with correct values", async () => {
+      const sut = makeSut();
+
+      jest.spyOn(jwt, "verify").mockImplementationOnce(async () => "any_value");
+      const verifySpy = jest.spyOn(jwt, "verify");
+
+      await sut.decrypt("any_token");
+
+      expect(verifySpy).toHaveBeenCalledWith("any_token", "secret");
+    });
   });
 });
--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -1,10 +1,16 @@
 import jwt from "jsonwebtoken";
+import { Decrypter } from "../../../data/protocols/criptography/decrypter";
 import { Encrypter } from "../../../data/protocols/criptography/encrypter";
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor(private readonly secret: string) {}
 
   async encrypt(value: string): Promise<string> {
     return await jwt.sign({ id: value }, this.secret);
+  }
+
+  async decrypt(value: string): Promise<string> {
+    await jwt.verify(value, this.secret);
+    return;
   }
 }
