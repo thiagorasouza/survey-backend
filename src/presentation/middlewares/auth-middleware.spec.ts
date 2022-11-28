6f3426092bbf438370e7c88e95ce1105d6b35950
Thiago Souza <thiagorasouza@gmail.com>
2022-11-28 18:45:43 -0300

    feat: ensure that AuthMiddleware calls LoadAccountByToken with correct accessToken

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,20 +1,54 @@
+import { AccountModel } from "../../domain/models/account";
+import { LoadAccountByToken } from "../../domain/usecases/load-account-by-token";
 import { AccessDeniedError } from "../errors/access-denied-error";
 import { forbidden } from "../helpers/http/http-helper";
+import { HttpRequest } from "../protocols";
 import { AuthMiddleware } from "./auth-middleware";
+
+const makeLoadAccountByToken = (): LoadAccountByToken => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load(): Promise<AccountModel> {
+      return makeFakeAccount();
+    }
+  }
+
+  return new LoadAccountByTokenStub();
+};
 
 interface SutTypes {
   sut: AuthMiddleware;
+  loadAccountByTokenStub: LoadAccountByToken;
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new AuthMiddleware();
-  return { sut };
+  const loadAccountByTokenStub = makeLoadAccountByToken();
+  const sut = new AuthMiddleware(loadAccountByTokenStub);
+  return { sut, loadAccountByTokenStub };
 };
+
+const makeFakeAccount = (): AccountModel => ({
+  id: "valid_id",
+  name: "valid_name",
+  email: "valid_email@mail.com",
+  password: "valid_password",
+});
 
 describe("Auth Middleware", () => {
   it("should return 403 if no X-Access-Token exists in headers", async () => {
     const { sut } = makeSut();
     const httpResponse = await sut.handle({});
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()));
+  });
+
+  it("should call LoadAccountByToken with correct accessToken", async () => {
+    const { sut, loadAccountByTokenStub } = makeSut();
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, "load");
+    const httpRequest: HttpRequest = {
+      headers: {
+        "X-Access-Token": "any_token",
+      },
+    };
+    await sut.handle(httpRequest);
+    expect(loadSpy).toHaveBeenCalledWith("any_token");
   });
 });
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,9 +1,18 @@
+import { LoadAccountByToken } from "../../domain/usecases/load-account-by-token";
 import { AccessDeniedError } from "../errors/access-denied-error";
 import { forbidden } from "../helpers/http/http-helper";
 import { HttpRequest, HttpResponse, Middleware } from "../protocols";
 
 export class AuthMiddleware implements Middleware {
+  constructor(private readonly loadAccountByToken: LoadAccountByToken) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    return forbidden(new AccessDeniedError());
+    const accessToken = httpRequest.headers?.["X-Access-Token"];
+    if (!accessToken) {
+      return forbidden(new AccessDeniedError());
+    }
+    const account = await this.loadAccountByToken.load(
+      httpRequest.headers["X-Access-Token"]
+    );
   }
 }
