7b7686c456d2419be73be1422c3c1240e296a79c
Thiago Souza <thiagorasouza@gmail.com>
2022-11-27 21:34:07 -0300

    feat: add AddSurvey composition

---
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.ts
@@ -0,0 +1,14 @@
+import { Validation } from "../../../../presentation/protocols";
+import {
+  RequiredFieldValidation,
+  ValidationComposite,
+} from "../../../../validation/validators";
+
+export const makeAddSurveyValidation = (): ValidationComposite => {
+  const validations: Validation[] = [];
+  for (const field of ["question", "answers"]) {
+    validations.push(new RequiredFieldValidation(field));
+  }
+  const validationComposite = new ValidationComposite(validations);
+  return validationComposite;
+};
---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,40 @@
+import { Collection } from "mongodb";
+import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
+import env from "../config/env";
+import request from "supertest";
+import app from "../config/app";
+
+describe("Survey Routes", () => {
+  let surveys: Collection;
+
+  beforeEach(async () => {
+    surveys = await MongoHelper.getCollection("surveys");
+    await surveys.deleteMany({});
+  });
+
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoUrl);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  describe("POST /surveys", () => {
+    it("should return 204 on add survey success", async () => {
+      await request(app)
+        .post("/api/surveys")
+        .send({
+          question: "Any Question",
+          answers: [
+            {
+              image: "http://image-name.com",
+              answer: "Answer 1",
+            },
+            { answer: "Answer 2" },
+          ],
+        })
+        .expect(204);
+    });
+  });
+});
