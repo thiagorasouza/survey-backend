abe829f6f72c2a6118e2dcd9b4588f3f977b3f5b
Thiago Souza <thiagorasouza@gmail.com>
2022-12-01 09:49:51 -0300

    test: ensure AccountMongoRepository returns null if loadByToken fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -115,5 +115,11 @@ describe("Account Mongo Repository", () => {
       expect(account.email).toBe("any_email@email.com");
       expect(account.password).toBe("any_password");
     });
+
+    it("should return null if loadByToken fails", async () => {
+      const sut = makeSut();
+      const account = await sut.loadByToken("any_token");
+      expect(account).toBeFalsy();
+    });
   });
 });
