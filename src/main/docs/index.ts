08c3b7fac47ea08e640ea16ff75ed46fea10e4e1
Thiago Souza <thiagorasouza@gmail.com>
2022-12-21 19:16:47 +0000

    docs: add load surveys api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,11 +1,17 @@
 import { badRequest } from "./components/bad-request";
+import { forbidden } from "./components/forbidden";
 import { notFound } from "./components/not-found";
 import { serverError } from "./components/server-error";
 import { unauthorized } from "./components/unauthorized";
 import { loginPath } from "./paths/login-path";
+import { surveysPath } from "./paths/surveys-path";
 import { accountSchema } from "./schemas/account-schema";
+import { apiKeyAuthSchema } from "./schemas/api-key-auth-schema";
 import { errorSchema } from "./schemas/error-schema";
 import { loginParamsSchema } from "./schemas/login-params-schema";
+import { surveyAnswerSchema } from "./schemas/survey-answer-schema";
+import { surveySchema } from "./schemas/survey-schema";
+import { surveysSchema } from "./schemas/surveys-schema";
 
 export default {
   openapi: "3.0.0",
@@ -23,19 +29,30 @@ export default {
     {
       name: "Login",
     },
+    {
+      name: "Surveys",
+    },
   ],
   paths: {
     "/login": loginPath,
+    "/surveys": surveysPath,
   },
   components: {
     badRequest: badRequest,
     serverError: serverError,
     unauthorized: unauthorized,
     notFound: notFound,
+    forbidden: forbidden,
+    securitySchemes: {
+      apiKeyAuth: apiKeyAuthSchema,
+    },
   },
   schemas: {
     error: errorSchema,
     account: accountSchema,
     loginParams: loginParamsSchema,
+    survey: surveySchema,
+    surveys: surveysSchema,
+    surveyAnswer: surveyAnswerSchema,
   },
 };
---
+++ src/main/docs/paths/surveys-path.ts
@@ -0,0 +1,32 @@
+export const surveysPath = {
+  get: {
+    security: [
+      {
+        apiKeyAuth: [],
+      },
+    ],
+    tags: ["Surveys"],
+    summary: "API to create and list surveys",
+    responses: {
+      200: {
+        description: "Success",
+        content: {
+          "application/json": {
+            schema: {
+              $ref: "#/schemas/surveys",
+            },
+          },
+        },
+      },
+      403: {
+        $ref: "#/components/forbidden",
+      },
+      404: {
+        $ref: "#/components/notFound",
+      },
+      500: {
+        $ref: "#/components/serverError",
+      },
+    },
+  },
+};
