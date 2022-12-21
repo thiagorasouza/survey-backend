84a7fc923d08d5ee69400dfb2b76482fe330cafe
Thiago Souza <thiagorasouza@gmail.com>
2022-12-21 17:53:18 +0000

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,29 @@
+import { loginPath } from "./paths/login-path";
+import { accountSchema } from "./schemas/account-schema";
+import { loginParamsSchema } from "./schemas/login-params-schema";
+
 export default {
   openapi: "3.0.0",
   info: {
     title: "Survey Backend API",
     description: "API for the survey app backend",
     version: "1.5.1",
+  },
+  servers: [
+    {
+      url: "/api",
+    },
+  ],
+  tags: [
+    {
+      name: "Login",
+    },
+  ],
+  paths: {
+    "/login": loginPath,
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema,
   },
 };
