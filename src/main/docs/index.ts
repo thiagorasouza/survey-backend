1e3c16c0ccb5323664a4509662a4eb02c33f38b4
Thiago Souza <thiagorasouza@gmail.com>
2022-12-22 11:00:58 +0000

    docs: add save survey result api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -5,14 +5,17 @@ import { serverError } from "./components/server-error";
 import { unauthorized } from "./components/unauthorized";
 import { loginPath } from "./paths/login-path";
 import { signupPath } from "./paths/signup-path";
+import { surveyResultPath } from "./paths/survey-result-path";
 import { surveysPath } from "./paths/surveys-path";
 import { accountSchema } from "./schemas/account-schema";
 import { addSurveyParamsSchema } from "./schemas/add-survey-schema";
 import { apiKeyAuthSchema } from "./schemas/api-key-auth-schema";
 import { errorSchema } from "./schemas/error-schema";
 import { loginParamsSchema } from "./schemas/login-params-schema";
+import { saveSurveyResultParamsSchema } from "./schemas/save-survey-result-schema";
 import { signupParamsSchema } from "./schemas/signup-params-schema";
 import { surveyAnswerSchema } from "./schemas/survey-answer-schema";
+import { surveyResultSchema } from "./schemas/survey-result-schema";
 import { surveySchema } from "./schemas/survey-schema";
 import { surveysSchema } from "./schemas/surveys-schema";
 
@@ -40,6 +43,7 @@ export default {
     "/login": loginPath,
     "/surveys": surveysPath,
     "/signup": signupPath,
+    "/surveys/{surveyId}/result": surveyResultPath,
   },
   components: {
     badRequest: badRequest,
@@ -60,5 +64,7 @@ export default {
     surveys: surveysSchema,
     surveyAnswer: surveyAnswerSchema,
     addSurveyParams: addSurveyParamsSchema,
+    saveSurveyResultParams: saveSurveyResultParamsSchema,
+    surveyResult: surveyResultSchema,
   },
 };
