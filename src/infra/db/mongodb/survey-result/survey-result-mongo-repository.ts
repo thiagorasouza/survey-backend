bda805d111dcdcb0be3dd197ab175a9b81ac0801
Thiago Souza <thiagorasouza@gmail.com>
2022-12-22 18:05:58 +0000

    fix: make SurveyResultMongoRepository implement the right interface

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -1,11 +1,9 @@
+import { SaveSurveyResultRepository } from "../../../../data/protocols/db/survey-result/save-survey-result-repository";
 import { SurveyResultModel } from "../../../../domain/models/survey-result";
-import {
-  SaveSurveyResult,
-  SaveSurveyResultParams,
-} from "../../../../domain/usecases/survey-result/save-survey-result";
+import { SaveSurveyResultParams } from "../../../../domain/usecases/survey-result/save-survey-result";
 import { MongoHelper } from "../helpers/mongo-helper";
 
-export class SurveyResultMongoRepository implements SaveSurveyResult {
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
   async save(data: SaveSurveyResultParams): Promise<SurveyResultModel> {
     const surveyResultsCollection = await MongoHelper.getCollection(
       "surveyResults"
