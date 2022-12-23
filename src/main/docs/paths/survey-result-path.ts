2c2a2546bb421c6a56e0f983af078adf2a09ddb5
Thiago Souza <thiagorasouza@gmail.com>
2022-12-23 12:40:25 +0000

    docs: add load survey result api docs

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -48,4 +48,44 @@ export const surveyResultPath = {
       },
     },
   },
+  get: {
+    security: [
+      {
+        apiKeyAuth: [],
+      },
+    ],
+    tags: ["Surveys"],
+    summary: "API to load survey compiled results",
+    parameters: [
+      {
+        in: "path",
+        name: "surveyId",
+        required: true,
+        schema: {
+          type: "string",
+        },
+      },
+    ],
+    responses: {
+      200: {
+        description: "Success",
+        content: {
+          "application/json": {
+            schema: {
+              $ref: "#/schemas/surveyResult",
+            },
+          },
+        },
+      },
+      403: {
+        $ref: "#/components/forbidden",
+      },
+      404: {
+        $ref: "#/components/notFound",
+      },
+      500: {
+        $ref: "#/components/serverError",
+      },
+    },
+  },
 };
