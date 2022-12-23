ad61a1ea74320f44538ecfec83279b94e35125b9
Thiago Souza <thiagorasouza@gmail.com>
2022-12-23 12:14:08 +0000

    feat: ensure that LoadSurveyResultController calls LoadSurveyById with correct value

---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -0,0 +1,107 @@
+import {
+  mockLoadSurveyById,
+  mockLoadSurveyResult,
+} from "../../../../domain/test";
+import { LoadSurveyResult } from "../../../../domain/usecases/survey-result/load-survey-result";
+import { LoadSurveyById } from "../../../../domain/usecases/survey/load-survey-by-id";
+import { HttpRequest } from "../../../protocols";
+import { LoadSurveyResultController } from "./load-survey-result-controller";
+import MockDate from "mockdate";
+import { forbidden } from "../../../helpers/http/http-helper";
+import { InvalidParamError } from "../../../errors";
+
+interface SutTypes {
+  sut: LoadSurveyResultController;
+  loadSurveyByIdStub: LoadSurveyById;
+  loadSurveyResultStub: LoadSurveyResult;
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdStub = mockLoadSurveyById();
+  const loadSurveyResultStub = mockLoadSurveyResult();
+  const sut = new LoadSurveyResultController(
+    loadSurveyByIdStub,
+    loadSurveyResultStub
+  );
+
+  return {
+    sut,
+    loadSurveyByIdStub,
+    loadSurveyResultStub,
+  };
+};
+
+const mockRequest = (): HttpRequest => ({
+  params: {
+    surveyId: "any_survey_id",
+  },
+  accountId: "any_account_id",
+});
+
+describe("LoadSurveyResultController", () => {
+  beforeAll(() => {
+    MockDate.set(new Date());
+  });
+
+  afterAll(() => {
+    MockDate.reset();
+  });
+
+  it("should call LoadSurveyById with correct value", async () => {
+    const { sut, loadSurveyByIdStub } = makeSut();
+
+    const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, "loadById");
+
+    const request = mockRequest();
+    await sut.handle(request);
+
+    expect(loadByIdSpy).toHaveBeenCalledTimes(1);
+    expect(loadByIdSpy).toHaveBeenCalledWith(request.params.surveyId);
+  });
+
+  // it("should return 403 if LoadSurveyById returns null", async () => {
+  //   const { sut, loadSurveyByIdStub } = makeSut();
+
+  //   jest
+  //     .spyOn(loadSurveyByIdStub, "loadById")
+  //     .mockReturnValueOnce(Promise.resolve(null));
+
+  //   const request = mockRequest();
+  //   const httpResponse = await sut.handle(request);
+
+  //   expect(httpResponse).toEqual(forbidden(new InvalidParamError("surveyId")));
+  // });
+
+  // it("should call LoadSurveyResult with correct value", async () => {
+  //   const { sut, loadSurveyResultStub } = makeSut();
+
+  //   const loadSpy = jest.spyOn(loadSurveyResultStub, "load");
+
+  //   const request = mockRequest();
+  //   await sut.handle(request);
+
+  //   expect(loadSpy).toHaveBeenCalledTimes(1);
+  //   expect(loadSpy).toHaveBeenCalledWith("any_survey_id");
+  // });
+
+  // it("should return 500 if LoadSurveyById throws", async () => {
+  //   const { sut, loadSurveyByIdStub } = makeSut();
+  //   jest
+  //     .spyOn(loadSurveyByIdStub, "loadById")
+  //     .mockReturnValueOnce(Promise.reject(new Error()));
+
+  //   const request = mockRequest();
+  //   const httpResponse = await sut.handle(request);
+
+  //   expect(httpResponse).toEqual(serverError(new Error()));
+  // });
+
+  // it("should return 200 on success", async () => {
+  //   const { sut } = makeSut();
+
+  //   const request = mockRequest();
+  //   const httpResponse = await sut.handle(request);
+
+  //   expect(httpResponse).toEqual(ok(mockSurveyCompiledModel()));
+  // });
+});
---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -0,0 +1,47 @@
+import { LoadSurveyResult } from "../../../../domain/usecases/survey-result/load-survey-result";
+import { LoadSurveyById } from "../../../../domain/usecases/survey/load-survey-by-id";
+import { Controller, HttpRequest, HttpResponse } from "../../../protocols";
+
+export class LoadSurveyResultController implements Controller {
+  constructor(
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly loadSurveyResult: LoadSurveyResult
+  ) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const { surveyId } = httpRequest.params;
+    const survey = await this.loadSurveyById.loadById(surveyId);
+    //   if (!survey) {
+    //     return forbidden(new InvalidParamError("surveyId"));
+    //   }
+    return;
+    // try {
+    //   const accountId = httpRequest.accountId;
+    //   const { surveyId } = httpRequest.params;
+    //   const { answer } = httpRequest.body;
+
+    //   const survey = await this.loadSurveyById.loadById(surveyId);
+    //   if (!survey) {
+    //     return forbidden(new InvalidParamError("surveyId"));
+    //   }
+
+    //   const surveyAnswers = survey.answers.map((item) => item.answer);
+    //   if (!surveyAnswers.includes(answer)) {
+    //     return forbidden(new InvalidParamError("answer"));
+    //   }
+
+    //   await this.saveSurveyResult.save({
+    //     accountId,
+    //     surveyId,
+    //     answer,
+    //     date: new Date(),
+    //   });
+
+    //   const surveyCompiled = await this.loadSurveyResult.load(surveyId);
+
+    //   return ok(surveyCompiled);
+    // } catch (error) {
+    //   return serverError(error);
+    // }
+  }
+}
