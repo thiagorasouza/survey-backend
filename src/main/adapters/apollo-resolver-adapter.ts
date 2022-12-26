eda1cc4c21b9f03d72aead3be4ec82828964d978
Thiago Souza <thiagorasouza@gmail.com>
2022-12-26 22:39:43 +0000

    feat: treat errors to return GraphQL error codes

--- src/main/adapters/apollo-resolver-adapter.ts
+++ src/main/adapters/apollo-resolver-adapter.ts
@@ -1,6 +1,39 @@
+import { GraphQLError } from "graphql";
 import { Controller } from "../../presentation/protocols";
 
-export const adaptResolver = async (controller: Controller, args: any) => {
+export const adaptResolver = async (
+  controller: Controller,
+  args: any
+): Promise<any> => {
   const httpResponse = await controller.handle(args);
-  return httpResponse.body;
+  switch (httpResponse.statusCode) {
+    case 200:
+    case 204:
+      return httpResponse.body;
+    case 400:
+      throw new GraphQLError(httpResponse.body.message, {
+        extensions: {
+          code: "BAD_USER_INPUT",
+        },
+      });
+    case 401:
+      throw new GraphQLError(httpResponse.body.message, {
+        extensions: {
+          code: "UNAUTHORIZED",
+        },
+      });
+    case 403:
+      throw new GraphQLError(httpResponse.body.message, {
+        extensions: {
+          code: "FORBIDDEN",
+        },
+      });
+    case 500:
+    default:
+      throw new GraphQLError(httpResponse.body.message, {
+        extensions: {
+          code: "INTERNAL_SERVER_ERROR",
+        },
+      });
+  }
 };
