10c9f127876f642952e41aa7d4fe8538c619b06d
Thiago Souza <thiagorasouza@gmail.com>
2022-12-28 23:21:23 +0000

    test: ensure that signup mutation returns access token and name for valid data

--- tests/main/graphql/login-query.test.ts
+++ tests/main/graphql/login-query.test.ts
@@ -5,13 +5,14 @@ import { Express } from "express";
 import * as bcrypt from "bcrypt";
 import env from "../../../src/main/config/env";
 import { setupServer } from "../../../src/main/config/server";
-import { AddAccountRequestModel } from "../../../src/domain/usecases";
+import { LoginRequest } from "../../../src/presentation/controllers/login-controller";
+import { SignUpRequest } from "../../../src/presentation/controllers/signup-controller";
 
-describe("Login Query", () => {
+describe("Login and SignUp APIs", () => {
   let app: Express;
   let accounts: Collection;
 
-  const makeLoginQuery = (email: string, password: string) => ({
+  const makeLoginQuery = (loginData: LoginRequest) => ({
     query: `#graphql
             query ($email: String!, $password: String!) {
               login (email: $email, password: $password) {
@@ -20,17 +21,34 @@ describe("Login Query", () => {
               }
             }
           `,
-    variables: { email, password },
+    variables: { ...loginData },
   });
 
-  const makeAccount = async (
-    account: AddAccountRequestModel
-  ): Promise<void> => {
+  const makeSignupQuery = (signUpData: SignUpRequest) => ({
+    query: `#graphql
+            mutation ($name: String!, $email: String!, $password: String!, $passwordConfirmation: String!) {
+              signup (name: $name, email: $email, password: $password, passwordConfirmation: $passwordConfirmation) {
+                accessToken
+                name
+              }
+            }
+          `,
+    variables: { ...signUpData },
+  });
+
+  const makeAccount = async (account: SignUpRequest): Promise<void> => {
     await accounts.insertOne({
       ...account,
       password: await bcrypt.hash(account.password, 12),
     });
   };
+
+  const mockAccount = (): SignUpRequest => ({
+    name: "valid_name",
+    email: "valid_email@email.com",
+    password: "valid_password",
+    passwordConfirmation: "valid_password",
+  });
 
   beforeEach(async () => {
     accounts = await MongoHelper.getCollection("accounts");
@@ -48,14 +66,13 @@ describe("Login Query", () => {
 
   describe("login() query", () => {
     it("should return access token and name for valid credentials", async () => {
-      const fakeData = {
-        name: "valid_name",
-        email: "valid_email@email.com",
-        password: "valid_password",
-      };
+      const fakeData = mockAccount();
 
       await makeAccount(fakeData);
-      const query = makeLoginQuery(fakeData.email, fakeData.password);
+      const query = makeLoginQuery({
+        email: fakeData.email,
+        password: fakeData.password,
+      });
 
       const result = await request(app).post("/graphql").send(query);
 
@@ -64,15 +81,40 @@ describe("Login Query", () => {
     });
 
     it("should return UNAUTHORIZED for invalid credentials", async () => {
-      const query = makeLoginQuery(
-        "invalid_email@email.com",
-        "invalid_password"
-      );
+      const query = makeLoginQuery({
+        email: "invalid_email@email.com",
+        password: "invalid_password",
+      });
 
       const result = await request(app).post("/graphql").send(query);
 
       expect(result.body?.data).toBeFalsy();
       expect(result.body?.errors[0].extensions.code).toBe("UNAUTHORIZED");
     });
+  });
+
+  describe("signup() mutation", () => {
+    it("should return access token and name for valid data", async () => {
+      const fakeData = mockAccount();
+
+      const query = makeSignupQuery(fakeData);
+
+      const result = await request(app).post("/graphql").send(query);
+
+      expect(result.body?.data?.signup?.accessToken).toBeTruthy();
+      expect(result.body?.data?.signup?.name).toBe(fakeData.name);
+    });
+
+    // it("should return UNAUTHORIZED for invalid credentials", async () => {
+    //   const fakeData = mockAccount();
+
+    //   const query = makeSignupQuery(fakeData);
+
+    //   const result = await request(app).post("/graphql").send(query);
+    //   console.log("🚀 ~ result", result.body)
+
+    //   expect(result.body?.data).toBeFalsy();
+    //   expect(result.body?.errors[0].extensions.code).toBe("UNAUTHORIZED");
+    // });
   });
 });
