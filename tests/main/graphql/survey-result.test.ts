2e0f8f17a8ea88afd968cc3463dd1ce8eded9aaf
Thiago Souza <thiagorasouza@gmail.com>
2022-12-29 01:10:08 +0000

    test: ensure that saveSurveyResult mutation returns compiled survey for valid data

--- tests/main/graphql/survey-result.test.ts
+++ tests/main/graphql/survey-result.test.ts
@@ -32,6 +32,25 @@ describe("SurveyResult GraphQL APIs", () => {
     variables: { surveyId },
   });
 
+  const makeSaveSurveyResultQuery = (surveyId: string, answer: string) => ({
+    query: `#graphql
+            mutation ($surveyId: String!, $answer: String!) {
+              saveSurveyResult (surveyId: $surveyId, answer: $answer) {
+                surveyId
+                question                
+                answers {
+                  image
+                  answer
+                  count
+                  percent
+                  isCurrentAccountAnswer
+                }
+                date
+              }
+            }`,
+    variables: { surveyId, answer },
+  });
+
   beforeEach(async () => {
     surveys = await MongoHelper.getCollection("surveys");
     await surveys.deleteMany({});
@@ -92,6 +111,43 @@ describe("SurveyResult GraphQL APIs", () => {
 
       expect(result.body?.data).toBeFalsy();
       expect(result.body?.errors[0].extensions.code).toBe("FORBIDDEN");
+    });
+  });
+
+  describe("saveSurveyResult() mutation", () => {
+    it("should return compiled survey result for valid data", async () => {
+      const surveyId = await makeSurvey();
+      const accessToken = await makeAccessToken();
+
+      const query = makeSaveSurveyResultQuery(surveyId, "Answer 1");
+      const result = await request(app)
+        .post("/graphql")
+        .set("X-Access-Token", accessToken)
+        .send(query);
+
+      expect(result.body?.data?.saveSurveyResult?.surveyId).toBe(surveyId);
+      expect(result.body?.data?.saveSurveyResult?.question).toBe(
+        "Any Question"
+      );
+      expect(result.body?.data?.saveSurveyResult?.answers).toEqual([
+        {
+          image: "http://image-name.com",
+          answer: "Answer 1",
+          count: 1,
+          percent: 100,
+          isCurrentAccountAnswer: false,
+        },
+        {
+          image: null,
+          answer: "Answer 2",
+          count: 0,
+          percent: 0,
+          isCurrentAccountAnswer: false,
+        },
+      ]);
+      expect(result.body?.data?.saveSurveyResult?.date).toBe(
+        new Date().toISOString()
+      );
     });
   });
 });
